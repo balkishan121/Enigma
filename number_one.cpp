2e357f2c33117bab70e9f9812eb661285f994bfa
Balkishan Padihar <98443289+balkishan121@users.noreply.github.com>
2023-03-11 15:06:38 +0530

    Add files via upload

---
+++ number_one.cpp
@@ -0,0 +1,27 @@
+/*Write a program to print the numbers from 1 to 100, but replace
+multiples of 3 with "Fizz" and multiples of 5 with "Buzz".*/
+#include<stdio.h>
+int main()
+{
+	int i;
+	for(i=1;i<=100;++i)
+	{
+		if(i%3==0&&i%5==0)
+		{
+			printf("Fizz Buzz\n");
+		}
+		else if(i%3==0)
+		{
+			printf("Fizz\n");
+		}
+		 else if(i%5==0)
+		{
+			printf("Buzz\n");
+		}
+		else
+		{
+			printf("%d\n",i);
+		}
+		
+	}
+}
